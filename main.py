2fc6c2b234ecdcaf5a64216b9c568d68f5b94a2a
gaofan <1849121296@qq.com>
2021-11-15 21:45:56 +0800

    Some codes

--- main.py
+++ main.py
@@ -28,8 +28,9 @@ def get_html_text(url):
 def download_video(url, filename):
     my_video = urlretrieve(url=url, filename=filename)
 
-
-url = "https://f.video.weibocdn.com/o0/pJPWsp3Olx07Rl2uAQH601041205xymo0E020.mp4?label=mp4_1080p&template=1920x1080.25.0&trans_finger=d88af6227b88881484d2b59dfbafa136&media_id=4702789843222651&tp=8x8A3El:YTkl0eM8&us=0&ori=1&bf=3&ot=h&ps=3lckmu&uid=3ZoTIp&ab=3915-g1,1493-g0,1192-g0,1191-g0,1258-g0,3601-g15&Expires=1636982519&ssig=ZgVBIXCi1x&KID=unistore,video"
+#Expires
+#ssig=A9v8k%2FGjLF
+url = "https://f.video.weibocdn.com/o0/pJPWsp3Olx07Rl2uAQH601041205xymo0E020.mp4?label=mp4_1080p&template=1920x1080.25.0&trans_finger=d88af6227b88881484d2b59dfbafa136&media_id=4702789843222651&tp=8x8A3El:YTkl0eM8&us=0&ori=1&bf=3&ot=h&ps=3lckmu&uid=3ZoTIp&ab=3915-g1,1493-g0,1192-g0,1191-g0,1258-g0,3601-g15&Expires=1636987177&ssig=ucPzsd0inD&KID=unistore,video"
 filename = "G:\\Code\\My_Money_Project\\Video\\test.mp4"
 download_video(url, filename)
 print("@@@@@@@@下载结束@@@@@@")
